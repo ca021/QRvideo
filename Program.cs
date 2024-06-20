b8fc7e1db8f2b071d706ef67f95ed4b73a879577
cmandree <cmandree@my.okcu.edu>
2024-06-20 17:13:23 -0500

    Added files

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+using QRCoder;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        QRCodeGenerator qrGenerator = new QRCodeGenerator();
+        QRCodeData qrCodeData = qrGenerator.CreateQrCode("https://cmandree.github.io/video/", QRCodeGenerator.ECCLevel.Q);
+        QRCode qrCode = new QRCode(qrCodeData);
+        Bitmap qrCodeImage = qrCode.GetGraphic(20);
+        qrCodeImage.Save("qrCode.png");
+    }
+}
